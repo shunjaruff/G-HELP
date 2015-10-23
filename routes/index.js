0ba341a873e3882750360b8c0c6d76d4abbf9793
Minhaj Shunjaruff <shunjaruff@gmail.com>
2015-10-23 01:31:07 -0230

    Prototype update
    Added prototype for Assign Mentor page

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,11 @@ router.get('/registermentor', function(req, res, next) {
 router.get('/addevent', function(req, res, next) {
 	  res.render('addevent', { title: 'Add Event' });
 	});
+router.get('/assignmentor', function(req, res, next) {
+	  res.render('assignmentor', { title: 'Assign Mentor' });
+	});
+router.get('/test', function(req, res, next) {
+	  res.render('test', { title: 'Test' });
+	});
 
 module.exports = router;
