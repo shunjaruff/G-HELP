fa36aef5a59c9fccf923f0fd9847f734dcfad7e8
tosicky <tosingeeboy@yahoo.com>
2015-11-19 20:41:50 -0330

    Collections
    Student and Mentor collections done

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/profile', function(req, res) {
+    res.render('profile.jade');
+});
+
 router.get('/ghelp', function(req, res, next) {
   res.render('ghelp', { title: 'G-HELP' });
 });
-router.get('/registermentor', function(req, res, next) {
-	  res.render('registermentor', { title: 'Register Mentor' });
+router.get('/regmentor', function(req, res, next) {
+	  res.render('regmentor', { title: 'Register Mentor' });
 	});
+
+router.get('/regstud', function(req, res, next) {
+    res.render('regstud', { title: 'Register Student' });
+});
+
 router.get('/addevent', function(req, res, next) {
 	  res.render('addevent', { title: 'Add Event' });
 	});
@@ -38,31 +47,31 @@ router.get('/hostfamily', function(req, res, next) {
 	});
 
 /* POST to Add User Service */
-router.post('/registermentor', function(req, res) {
+router.post('/regmentor', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
 
     // Get our form values. These rely on the "name" attributes
-    var firstName = req.body.givenname;
-    var lastName = req.body.surname;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
     var major = req.body.major;
-    var userEmail = req.body.email;
-    var cellPhone = req.body.cellphone;
+    var email = req.body.email;
+    var mobileNo = req.body.mobile;
     var preference = req.body.preference;
-    var sex = req.body.optionsRadios;
+    var sex = req.body.sex;
     
 
     // Set our collection
-    var collection = db.get('usercollection');
+    var collection = db.get('mentor');
 
     // Submit to the DB
     collection.insert({
-        "firstname" : firstName,
-        "lastname" : lastName,
+        "firstName" : firstName,
+        "lastName" : lastName,
         "major" : major,
-        "email" : userEmail,
-        "phone" : cellPhone,
+        "email" : email,
+        "mobile" : mobileNo,
         "preference" : preference,
         "sex" : sex
         
@@ -74,7 +83,48 @@ router.post('/registermentor', function(req, res) {
         }
         else {
             // And forward to success page
-            res.send("Registeration successfull!");
+            res.redirect('/profile.jade');
+        }
+    });
+});
+
+router.post('/regstud', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var major = req.body.major;
+    var email = req.body.email;
+    var mobileNo = req.body.mobile;
+    var preference = req.body.preference;
+    var sex = req.body.sex;
+
+
+    // Set our collection
+    var collection = db.get('student');
+
+    // Submit to the DB
+    collection.insert({
+        "firstName" : firstName,
+        "lastName" : lastName,
+        "major" : major,
+        "email" : email,
+        "mobile" : mobileNo,
+        "preference" : preference,
+        "sex" : sex
+
+
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect('/profile');
         }
     });
 });
